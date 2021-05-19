1fe1e1f8531a7f4130f0771794bce03318b7c2bc
sdr <sdr@knowis.de>
2021-05-19 18:39:17 +0200

    adding ConfirmedInfections component with style

--- src/api/districtsApi/DistrictsApiService.ts
+++ src/api/districtsApi/DistrictsApiService.ts
@@ -48,6 +48,33 @@ class DistrictsApiService {
       return mappedData;
     }
   );
+
+  public getDistrictHistory = new ReadApiState<
+    IDistrictsResponse,
+    Error,
+    [string]
+  >(async (ags: string) => {
+    const url = `${getDistrictsURL()}/history/${ags}`;
+    const data = await sendRequest<IDistrictsApiResponse>(request.get(url));
+
+    /** mapped data to IDistrictsResponse from objects IDistrictsApiResponse returned from API*/
+    const mappedData: IDistrictsResponse = {
+      ags: data[ags].ags,
+      cases: data[ags].cases,
+      casesPer100k: data[ags].casesPer100k,
+      casesPerWeek: data[ags].casesPerWeek,
+      county: data[ags].county,
+      deaths: data[ags].deaths,
+      deathsPerWeek: data[ags].deathsPerWeek,
+      delta: data[ags].delta,
+      name: data[ags].name,
+      population: data[ags].population,
+      recovered: data[ags].recovered,
+      weekIncidence: data[ags].weekIncidence,
+    };
+
+    return mappedData;
+  });
 }
 
 export let districtsApiService = new DistrictsApiService();
--- src/components/confirmedInfections/ConfirmedInfections.tsx
+++ src/components/confirmedInfections/ConfirmedInfections.tsx
@@ -20,23 +20,34 @@ const ConfirmedInfections = (props: ConfirmedInfectionsProps) => {
     return null;
   }
 
+  const toDecimal = (number: string): string => {
+    const value = Number(parseFloat(number).toFixed(2));
+    return Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(
+      value
+    );
+  };
+
   return (
     <ReadApiStateContext state={districtsApiService.getDistrict}>
       {(data: IDistrictsResponse) => {
         return (
-          <>
-            <Accordion styled className="district-accordion">
+          <div className="district-accordion">
+            <Accordion styled>
               <h5>Confirmed New Infections</h5>
-              7-day Average {data.casesPerWeek}
-              Total {data.cases}
+              <p className="title">7-day Average</p>
+              <p>{toDecimal(data.casesPerWeek)}</p>
+              <p className="title">Up to yesterday</p>
+              <p>{toDecimal(data.weekIncidence)}</p>
+              <p className="title">Total</p>
+              <p>{toDecimal(data.cases)}</p>
             </Accordion>
             <Accordion styled className="district-accordion">
               <h5>7-day Incidence</h5>
-              up to yesterday 7-day Incidence {data.weekIncidence}
-              {data.casesPer100k}
-              confirmed new infections 100.000 residents
+              <p className="title">Total</p>
+              <p>{toDecimal(data.casesPer100k)}</p>
+              <small>confirmed new infections per 100.000 residents</small>
             </Accordion>
-          </>
+          </div>
         );
       }}
     </ReadApiStateContext>
